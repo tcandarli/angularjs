c779363d25c5be7b3639112ba809997359e6442b
Taner Candarli <tcandarli@gmail.com>
2018-09-21 17:52:07 +0200

    Add files via upload

---
+++ AngularJS_JSON/app.js
@@ -0,0 +1,17 @@
+var app = angular.module('tanerApp', []);
+
+app.controller('ajaxController', ['$scope', '$http', function ($scope, $http) {
+
+    $scope.loadDataPromise = function () {
+
+        $http.get("data.json").then(function (response) {
+            console.log("Status: " + response.status);
+            console.log("Type: " + response.headers("content-type"));
+            $scope.products = response.data;
+
+        });
+        $scope.sortField = 'Category';
+
+    }
+
+}]);
